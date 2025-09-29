6d306381d053c881958f44b1b7ae2d9bbedd1e1c
Sangam Koundal <koundalsangam314@gmail.com>
2025-09-29 16:12:22 +0530

    bubble sort

--- sort.cpp
+++ sort.cpp
@@ -5,7 +5,8 @@ using namespace std;
 void selection_sort(){
     // minimum element of a n sized array is found and placed at the start 
     // process is repeated for a (n-1) sized array till all the elements are exhausted and the array is sorted
-    int a[6] = {1,4,2,5,2,8};
+    // time complexity- O(n^2) -> best,worst and average case
+    int a[6] = {4,1,3,9,7};
     int mini;
     int temp;
     for(int i=0;i<6;i++){
@@ -22,8 +23,35 @@ void selection_sort(){
     }
 }
 
+void bubble_sort(){
+    // the maximum element is bubbled to the top
+    // worst- O(n^2);
+    // best - O(n); -> if the array is already sorted
+
+    int arr[6] = {4,1,3,9,7,8};
+    int temp;
+    int flag=0;
+    for(int i=0;i<6;i++){
+        flag=0;
+        for(int j=0;j<6-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                flag=1;
+                temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        if (flag==0){
+            break;
+        }
+    }
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<' ';
+    }
+}
 
 int main(){
-    selection_sort();
+    //selection_sort();
+    bubble_sort();
     return 0;
 }
